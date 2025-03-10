16c41c2e0d152a89267d899ffd5bed9a1b41ffc0
lamia fekry <123817607+lamiafekry2003@users.noreply.github.com>
2025-03-10 11:35:17 +0200

    fix-authSlice

--- src/store/slices/authSlice.ts
+++ src/store/slices/authSlice.ts
@@ -1,42 +1,4 @@
-// import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-// interface User {
-//   first_name: string;
-//   last_name: string;
-//   role: string;
-// }
-
-// interface AuthState {
-//   user: User | null;
-//   token: string | null;
-// }
-
-// const initialState: AuthState = {
-//   user: typeof window !== "undefined" ? JSON.parse(localStorage.getItem("user") || "null") : null,
-//   token: typeof window !== "undefined" ? localStorage.getItem("token") : null,
-// };
-
-// const authSlice = createSlice({
-//   name: "auth",
-//   initialState,
-//   reducers: {
-//     login: (state, action: PayloadAction<{ token: string; user: User }>) => {
-//       state.token = action.payload.token;
-//       state.user = action.payload.user;
-//       localStorage.setItem("token", action.payload.token);
-//       localStorage.setItem("user", JSON.stringify(action.payload.user));
-//     },
-//     logout: (state) => {
-//       state.token = null;
-//       state.user = null;
-//       localStorage.removeItem("token");
-//       localStorage.removeItem("user");
-//     },
-//   },
-// });
-
-// export const { login, logout } = authSlice.actions;
-// export default authSlice.reducer;
 // .......................
 
 // import { createSlice, PayloadAction } from "@reduxjs/toolkit";
